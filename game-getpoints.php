cdbb758b98aeaae9029310645f4aabd074a15f34
Joshua Benner <bennerster@gmail.com>
2017-04-30 18:02:14 -0400

    Grab a Teams points

---
+++ game-getpoints.php
@@ -0,0 +1,21 @@
+<?php
+require_once "db.inc.php";
+
+process($_GET['teamid']);
+
+function process($teamid) {
+    // Connect to the database
+    $pdo = pdo_connect();
+
+    $sql =<<<SQL
+    SELECT points from Team where id=$teamid
+SQL;
+    $statement = $pdo->prepare($sql);
+    $statement->execute();
+    $points = $statement->fetch()['points'];
+    echo $points;
+    echo '<game status="yes" msg="' . $points . '"/>';
+    exit;
+
+
+}
