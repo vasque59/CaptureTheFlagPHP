ff6f86d2702d265e3b7688ac9263b05511016696
Joshua Benner <bennerster@gmail.com>
2017-04-30 18:12:57 -0400

    Update the Flag's location

--- game-updateflag.php
+++ game-updateflag.php
@@ -3,16 +3,14 @@ require_once "db.inc.php";
 echo '<?xml version="1.0" encoding="1.0" ?>';
 
 // Process in a function
-getStatus($_GET['flagLat'],$_GET['flagLong'],$_GET['teamid']);
+updateFlagLoc($_GET['flagLat'],$_GET['flagLong'],$_GET['teamid']);
 
-
-function getStatus($flagLat, $flagLong, $teamid) {
+function updateFlagLoc($flagLat, $flagLong, $teamid) {
 //    echo "<p>$flagLat</p><p>$flagLong</p><p>$teamid</p>";
     $pdo = pdo_connect();
     $sql =<<<SQL
     UPDATE Team SET flagLat=$flagLat, flagLong=$flagLong WHERE id=$teamid
 SQL;
-    echo "<p>$sql</p>";
     $statement = $pdo->prepare($sql);
     $statement->execute();
 
