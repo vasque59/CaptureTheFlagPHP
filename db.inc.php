eb6679a3033c32082221320cc8f8656756642a19
vasque59 <vasquez_edward@hotmail.com>
2017-04-30 21:27:22 -0400

    taking my password out

--- db.inc.php
+++ db.inc.php
@@ -5,7 +5,7 @@ function pdo_connect() {
         // Production server
         $dbhost="mysql:host=mysql-user.cse.msu.edu;dbname=vasque59";
         $user = "vasque59";
-        $password = "Horacio1";
+        $password = "your password";
         return new PDO($dbhost, $user, $password);
     } catch(PDOException $e) {
         die( "Unable to select database");
