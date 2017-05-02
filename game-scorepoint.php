f0f54534aa98747fd4c825f646dd7d3593b9c777
Joshua Benner <bennerster@gmail.com>
2017-05-02 14:35:28 -0400

    Fix so flag is reset once you score

--- game-scorepoint.php
+++ game-scorepoint.php
@@ -10,7 +10,7 @@ function scorePoint($teamid)
     // Connect to the database
     $pdo = pdo_connect();
     $sql =<<<SQL
-    UPDATE Team SET points=points + 1 WHERE color=$teamid
+    UPDATE Team SET points=points + 1 isFlagPickedUp=0 WHERE color=$teamid
 SQL;
     $statement = $pdo->prepare($sql);
     $statement->execute();
